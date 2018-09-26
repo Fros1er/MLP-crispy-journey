fcfd5d022eac000b16fba54e00eced17a491a609
Fros1er <34234343+Fros1er@users.noreply.github.com>
2018-09-26 17:54:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pandas import read_csv
-from functions import test_ensemble, test_standardscaler, KNN_params, SVM_params, test_standardscaler_ensemble
+from functions import get_result, SVC, GBM_params, KNN_params, SVM_params, test
 from sklearn.model_selection import train_test_split
 
 names = [i for i in range(0,60)]
@@ -11,10 +11,15 @@ X = array[:, 0:60]
 Y = array[:, 60]
 num_folds = 10
 seed = 7
-validation_size = 0.2
+validation_size = 0.33
 X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
-test_ensemble(X_train, Y_train, num_folds, seed)
-test_standardscaler_ensemble(X_train, Y_train, num_folds, seed)
+
+
+'''
+GBM_params(X_train, Y_train, num_folds, seed)
 KNN_params(X_train, Y_train, num_folds, seed)
 SVM_params(X_train, Y_train, num_folds, seed)
+'''
+
+get_result(SVC(C=1.5, kernel='rbf'), X_train, Y_train, X_validation, Y_validation)
